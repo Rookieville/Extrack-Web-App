69c1623d1c52ad1d425f83bac7a19cfc9eec8122
Jake <asamoahjake77@gmail.com>
2023-03-31 10:34:12 +0000

    updates done by...

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '4--(d0^o%3vqt#-c(hf+8)a$95z8gbo57xol5pft!%xpve9_zd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"),]
 
 
